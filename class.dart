e66c6128469ff860102ca09c165f264f5fa59f0c
chembeu <simon.shanga11@gmail.com>
2025-02-07 16:13:06 +0300

    class

---
+++ class.dart
@@ -0,0 +1,10 @@
+class Student {
+  String? name;
+  int? age;
+  int? grade;
+  void info() {
+    print(name);
+    print(age);
+    print(grade);
+  }
+}
---
+++ loop.dart
@@ -0,0 +1,9 @@
+void main(){
+    var age= 2;
+    if(age>=18){
+        print("You are old");
+}
+else{
+    print("You are young");
+}
+}
