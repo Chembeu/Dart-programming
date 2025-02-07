d4041ee58045915945b1f39e8d3815f7e231b332
chembeu <simon.shanga11@gmail.com>
2025-02-07 16:21:40 +0300

    object

--- class.dart
+++ class.dart
@@ -8,3 +8,22 @@ class Student {
     print(grade);
   }
 }
+
+class Book {
+  String? name;
+  String? author;
+  int? price;
+  void display() {
+    print(name);
+    print(author);
+    print(price);
+  }
+}
+
+void main() {
+  Book book = Book();
+  book.name = "Animal farm";
+  book.author = "Simon";
+  book.price = 1200;
+  book.display();
+}
