2350c894acac2b34dc82e02cdccea71eb8345662
chembeu <simon.shanga11@gmail.com>
2025-02-11 15:40:33 +0300

    arrow_fuction

---
+++ arrow_fuction.dart
@@ -0,0 +1,6 @@
+double calculatearea(double length, double width) => length * width;
+void main() {
+  double length = 10;
+  double width = 20;
+  print("Area of rectangle is ${calculatearea(length, width)}");
+}
